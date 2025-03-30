956c94deb48d999e9a9a0917c2cac448143475cd
ryanfelipp <ryan.bandeira1@gmail.com>
2025-03-30 07:43:12 -0300

    Adição de função para input das notas

--- programanota1.py
+++ programanota1.py
@@ -1,8 +1,16 @@
-nota1 = float(input('Digite sua primeira nota: '))
-nota2 = float(input('Digite sua segunda nota: '))
-nota3 = float(input('Digite sua terceira nota: '))
+boletim = []
 
-media = (nota1+nota2+nota3)/3
+def coletar_nota(qtd_notas):
+    for i in range (1,4):
+        nota = float(input(f'Digite a sua {i} nota: '))
+        while nota >10 or nota< 0:
+            nota = float(input(f'Nota Invalida a sua {i} nota: '))
+        boletim.append(nota)
+    print(f"Suas notas foram\n{boletim}")
+
+coletar_nota(3)   
+
+media = (boletim[0]+boletim[1]+boletim[2])/3
 
 if media <=10 and media >= 7:
     print("Aluno Aprovado!")
